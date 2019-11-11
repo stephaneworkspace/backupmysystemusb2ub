d97e743ccd9ffa943149134871b3a8b3c1279a74
stephaneworkspace <s.bressani@bluewin.ch>
2019-11-11 23:46:55 +0100

    Add pipe to sh

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 # Author: Stéphane Bressani <s.bressani@bluewin.ch>
 # Backup usb 2 usb with same size complete
 cd /home/stephane/Code/Python/backupmysystemusb2usb/
-/usr/bin/python3 /home/stephane/Code/Python/backupmysystemusb2usb/run.py
+rm /home/stephane/Temp/BackupMySytem-Temp.log
+touch /home/stephane/Temp/BackupMySytem-Temp.log
+/usr/bin/python3 /home/stephane/Code/Python/backupmysystemusb2usb/run.py > /home/stephane/Temp/BackupMySytem-Temp.log
