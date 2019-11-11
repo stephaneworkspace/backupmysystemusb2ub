db1074eef8a9c361b4b33455711151d43019e48d
stephaneworkspace <s.bressani@bluewin.ch>
2019-11-11 23:16:14 +0100

    Add full path for script automatisatin, i will use yml later

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Author: Stéphane Bressani <s.bressani@bluewin.ch>
 # Backup usb 2 usb with same size complete
-/usr/bin/python3 run.py
+/usr/bin/python3 /home/stephane/Code/Python/backupmysystemusb2usb/run.py
--- run_user.sh
+++ run_user.sh
@@ -2,4 +2,4 @@
 # Author: Stéphane Bressani <s.bressani@bluewin.ch>
 # Backup usb 2 usb with same size complete
 export DISPLAY=:0.0
-/usr/bin/python3 run_user.py
+/usr/bin/python3 /home/stephane/Code/Python/backupmysystemusb2usb/run_user.py
