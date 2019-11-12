3982df5de7faf8b4c411b542207ed646b8ed0b2e
stephaneworkspace <s.bressani@bluewin.ch>
2019-11-12 07:22:50 +0100

    fix text

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author: Stéphane Bressani <s.bressani@bluewin.ch>
-# Backup usb 2 usb with same size complete
+# Backup usb 2 usb with the same size complete
 cd /home/stephane/Code/Python/backupmysystemusb2usb/
 rm /home/stephane/Temp/BackupMySytem-Temp.log
 touch /home/stephane/Temp/BackupMySytem-Temp.log
-/usr/bin/python3 /home/stephane/Code/Python/backupmysystemusb2usb/run.py > /home/stephane/Temp/BackupMySytem-Temp.log
+/home/stephane/Code/Python/backupmysystemusb2usb/run.py > /home/stephane/Temp/BackupMySytem-Temp.log
